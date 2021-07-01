7d99baafbdc5601c0dcf273e35af7af4bd33f169
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-01 09:36:27 -0700

    Added:Delete the last element in the linked list

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -2,70 +2,86 @@ package linkedlist;
 
 public class LinkedList
 {
-  Node head;
-  class Node
-  {
-	int data;
-    Node next;
+	Node head;
+	Node tail;
+	
+	 class Node
+	  {
+		 int data;
+		 Node next;
 		 
-  public Node(int data) 
-  {
-	this.data = data;
-	this.next = null;
-  }
-}
-public  Node insert (int data)
-{
-			
-	Node newNode = new Node(data);
-	if (head == null)
-	{
-		head = newNode;
-	}
-	else 
-	{
-		Node temp = head;
-		while(temp.next != null)
-		{
-			temp = temp.next;
+		 public Node(int data) 
+		 {
+			 this.data = data;
+			 this.next = null;
 		 }
-			temp.next = newNode;
-	 }
-			return newNode;
-}
-
-public void print() 
-{
-	Node temp = head;
-	while(temp!=null) 
-	{
-		System.out.print(temp.data + "->");
-		temp = temp.next;
-	}
-		System.out.println();
-}
-public void insertAfter(Node prevNode, int value)
-{
-   if (prevNode == null)
-	{
-		System.out.println("Previous Node should not be NULL");
-		return;
-	}
+	  }
+	public  Node insert (int data)
+	 {
 			
-	 Node newNode = new Node(value);
-	 newNode.next = prevNode.next;
-	 prevNode.next = newNode;
+			Node newNode = new Node(data);
+			if (head == null)
+			{
+				head = newNode;
+			}
+			else 
+			{
+				Node temp = head;
+				while(temp.next != null)
+				{
+					temp = temp.next;
+				}
+				temp.next = newNode;
+			}
+			return newNode;
+	 }
 
-}
-public int pop()
-{
-	int popData = 0;
-	if (head == null) 
-	{
-	System.out.println("Stack Over Flow");
-	}
-	popData = head.data;
-	head = head.next;
-	return popData;
-	}
+		public void print() 
+		{
+			Node temp = head;
+			while(temp!=null) 
+			{
+				System.out.print(temp.data + "->");
+				temp = temp.next;
+			}
+			System.out.println();
+		}
+		public void insertAfter(Node prevNode, int value)
+		{
+
+			if (prevNode == null)
+			{
+			System.out.println("Previous Node should not be NULL");
+			return;
+			}
+			
+			Node newNode = new Node(value);
+			newNode.next = prevNode.next;
+			prevNode.next = newNode;
+
+		}
+
+		 public int popLast()
+		  {
+		        if (head == null) 
+		        {
+		            System.out.println("Empty");
+		        } 
+		        else if (head.next == null) 
+		        {
+		            head = null;
+		        }
+		        else
+		        {
+		            Node n = head;
+		            Node n1 = head;
+		            while (n.next != null)
+		            {
+		                n1 = n;
+		                n = n.next;
+		            }
+		            n1.next = null;
+		        }
+				return 0;
+		   }
 }
--- src/linkedlist/Node.java
+++ src/linkedlist/Node.java
@@ -5,14 +5,15 @@ public class Node
     public static void main(String[] args)
 	 {
        System.out.println("Welcome to Data Structure");
+       
        LinkedList linkedList = new LinkedList();
 	   linkedList.insert(56);
 	   linkedList.insert(30);
 	   linkedList.insert(70);
 	   linkedList.print();
 	   
-	   int pop = linkedList.pop();
-		 System.out.println(pop);
+	   int popLast= linkedList.popLast();
+		 System.out.println(popLast);
 	   
 	   linkedList.print();
 
