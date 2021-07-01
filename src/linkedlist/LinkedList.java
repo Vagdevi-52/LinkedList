ec542077ed1c906c67c2baf5941381982d6c68d1
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-01 03:35:13 -0700

    Added:Create a simple Linked List

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -2,8 +2,44 @@ package linkedlist;
 
 public class LinkedList
 {
-	public static void main(String[] args)
+	Node head;
+	
+   class Node
+   {
+		 int data;
+		 Node next;
+		 
+	 public Node(int data) 
+	 {
+		this.data = data;
+		this.next = null;
+	 }
+   }
+ public void insert (int data)
+ {
+	Node newNode = new Node(data);
+	if (head == null)
 	{
-      System.out.println("welcome to LinkedList program");
-     }
+		head = newNode;
+	 }
+	else 
+	{
+		Node temp = head;
+		while(temp.next != null)
+		{
+		  temp = temp.next;
+		}
+		  temp.next = newNode;
+	}
+ }
+ public void print() 
+  {
+	 Node temp = head;
+	 while(temp!=null) 
+	 {
+		System.out.print(temp.data+"->");
+		temp = temp.next;
+	  }
+			System.out.println();
+		}
 }
---
+++ src/linkedlist/Node.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+public class Node {
+
+	public static void main(String[] args)
+	{
+       System.out.println("Welcome to Data Structure");
+	   LinkedList linkedList = new LinkedList();
+	   linkedList.insert(56);
+	   linkedList.insert(30);
+	   linkedList.insert(70);
+	   linkedList.print();
+
+	}
+
+}
