f042aefbbd779ef6bcad7f9b50aa1431bb6f16f7
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-02 01:47:01 -0700

    Added:Insert an element to the Linked List

--- src/linkedlist/Node.java
+++ src/linkedlist/Node.java
@@ -9,6 +9,7 @@ public class Node
        LinkedList linkedList = new LinkedList();
 	   linkedList.insert(56);
 	   linkedList.insert(30);
+	   linkedList.insert(40);
 	   linkedList.insert(70);
 	   linkedList.print();
 	   linkedList.findElement(30);
